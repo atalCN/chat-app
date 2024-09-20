a6f167470a27a6f2c1dafe17eec4e7c6aae8c3bc
atal98 <80337520+atal98@users.noreply.github.com>
2024-09-20 10:40:46 +0530

    update code

--- chat/consumers.py
+++ chat/consumers.py
@@ -30,94 +30,45 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.send(text_data = json.dumps({"message":message ,"username":username}))
 
 
-# consumers.py
 
-# class ChatConsumer(AsyncWebsocketConsumer):
+# class VideoChatConsumer(AsyncWebsocketConsumer):
 #     async def connect(self):
-#         # Create a room name based on the usernames of the two users
-#         self.room_name = self.scope['url_route']['kwargs']['room_name']
-#         self.room_group_name = f"chat_{self.room_name}"
-
-#         # Add the user to the WebSocket room group
+#         self.room_name = 'video_chat_room'
+#         self.room_group_name = 'chat_%s' % self.room_name
+#
+#         # Join room group
 #         await self.channel_layer.group_add(
 #             self.room_group_name,
 #             self.channel_name
 #         )
 #         await self.accept()
-
+#
 #     async def disconnect(self, close_code):
-#         # Leave the room group on disconnect
+#         # Leave room group
 #         await self.channel_layer.group_discard(
 #             self.room_group_name,
 #             self.channel_name
 #         )
-
+#
+#     # Receive message from WebSocket
 #     async def receive(self, text_data):
 #         text_data_json = json.loads(text_data)
 #         message = text_data_json['message']
-#         username = text_data_json['username']
-        
+#
 #         # Send message to room group
 #         await self.channel_layer.group_send(
 #             self.room_group_name,
 #             {
-#                 'type': 'send_message',
-#                 'message': message,
-#                 'username': username,
+#                 'type': 'chat_message',
+#                 'message': message
 #             }
 #         )
-
-#     # Send message to WebSocket
-#     async def send_message(self, event):
+#
+#     # Receive message from room group
+#     async def chat_message(self, event):
 #         message = event['message']
-#         username = event['username']
+#
+#         # Send message to WebSocket
 #         await self.send(text_data=json.dumps({
-#             'message': message,
-#             'username': username,
+#             'message': message
 #         }))
-
-
-
-      
-
-class VideoChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = 'video_chat_room'
-        self.room_group_name = 'chat_%s' % self.room_name
-
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
--- chat/routing.py
+++ chat/routing.py
@@ -1,10 +1,10 @@
 from django.urls import path , include, re_path
-from chat.consumers import ChatConsumer, VideoChatConsumer
+from chat.consumers import ChatConsumer
 
 # Here, "" is routing to the URL ChatConsumer which 
 # will handle the chat functionality.
 websocket_urlpatterns = [
     path("" , ChatConsumer.as_asgi()) , 
     # re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/chat/$', VideoChatConsumer.as_asgi()),
+    # re_path(r'ws/chat/$', VideoChatConsumer.as_asgi()),
 ] 
